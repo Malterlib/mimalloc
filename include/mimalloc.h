e35025f6c3e96bd8fd9f11e9f59508942be6c4a8
Erik Olofsson <erik@olofsson.info>
2019-10-17 10:55:52 +0200

    Expose mi_process_done

--- include/mimalloc.h
+++ include/mimalloc.h
@@ -155,6 +155,7 @@ mi_decl_export void mi_stats_print(void* out) mi_attr_noexcept;  // backward com
 mi_decl_export void mi_stats_print_out(mi_output_fun* out, void* arg) mi_attr_noexcept;
 
 mi_decl_export void mi_process_init(void)     mi_attr_noexcept;
+mi_decl_export void mi_cdecl mi_process_done(void)     mi_attr_noexcept;
 mi_decl_export void mi_thread_init(void)      mi_attr_noexcept;
 mi_decl_export void mi_thread_done(void)      mi_attr_noexcept;
 mi_decl_export void mi_thread_stats_print_out(mi_output_fun* out, void* arg) mi_attr_noexcept;
--- include/mimalloc/internal.h
+++ include/mimalloc/internal.h
@@ -111,7 +111,6 @@ static inline uintptr_t _mi_random_shuffle(uintptr_t x);
 extern mi_decl_hidden mi_decl_cache_align const mi_page_t  _mi_page_empty;
 void          _mi_process_load(void);
 
-void mi_cdecl _mi_process_done(void);
 bool          _mi_is_redirected(void);
 bool          _mi_allocator_init(const char** message);
 void          _mi_allocator_done(void);
